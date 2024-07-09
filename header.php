d8a1decf3e411f2c7477f58c1ec00a751efbbf8b
Aathmiga <aathmiga24@gmail.com>
2024-07-09 09:40:28 +0530

    homepage

---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARDHAS SCHOOL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-..." crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<style>
+ .navbar-scrolled{
+    background-color:#000;
+}
+
+        .dropdown-menu {
+            z-index: 1000; /* Ensure dropdown menu appears above everything else */
+        }
+</style>
+<body class="vh-100 over-flow-hidden">
+    <nav class="navbar navbar-expand-lg fixed-top border-bottom navbar-scrolled">
+        <div class="container">
+          <a class="navbar-brand text-white" href="#" style="font-weight: bold;">ARDHAS SCHOOL</a>
+          <button class="navbar-toggler text-black" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon text-black"></span>
+          </button>
+          <div class="offcanvas offcanvas-end text-black" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header text-black border-bottom">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">ARDHASS SCHOOL</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link active text-white"  href="#">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active text-white" href="#">Contact</a>
+                  </li>
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Gallery
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Alumini Meet</a></li>
+                    <li><a class="dropdown-item" href="#">Annual Day</a></li>
+                    <li><a class="dropdown-item" href="#">Sports Day</a></li>
+                    <li><a class="dropdown-item" href="#">Celebrations</a></li>
+
+                    
+                  </ul>
+                </li>
+            </ul>
+                  <div class="d-flex justify-content-center align-items-center gap-3">
+                    <a href="#" class=" btn btn-primary text-decoration-none px-3 py-1 rounded-2">LOGIN</a>
+                  </div>
+
+               
+            </div>
+          </div>
+        </div>
+      </nav>
+
+
+     
+
+</body>
+
+
+</html>
