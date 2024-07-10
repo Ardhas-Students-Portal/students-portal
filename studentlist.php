f7556dd2d9019156cda00afe4e2a4cd89d369fb3
praveen8074 <praveenb8074@gmail.com>
2024-07-10 11:11:41 +0530

    praveen

--- studentlist.php
+++ studentlist.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>studentlist</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <style>
@@ -55,7 +55,6 @@
         }
     </style>
 </head>
-
 <body>
     <div class="main-container d-flex">
         <div class="sidebar" id="side_nav">
