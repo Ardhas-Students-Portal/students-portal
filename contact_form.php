584bf34b0ef75a065d4f0f1a6123224b4b2914a4
Aathmiga <aathmiga24@gmail.com>
2024-07-10 10:29:55 +0530

    updated db

--- contact_form.php
+++ contact_form.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "students_portal";
+$dbname = "studentdb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname,3306);
