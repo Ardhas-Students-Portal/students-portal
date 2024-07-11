8d35e0575f49b6c1c78ba8c798c7d14675ee1b21
Aathmiga <aathmiga24@gmail.com>
2024-07-11 17:34:29 +0530

    Aathmiga commit

--- home.php
+++ home.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ARDHAS SCHOOL</title>
     <link rel="stylesheet" href="style.css">
-   
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
+ 
 </head>
 <style>
 
